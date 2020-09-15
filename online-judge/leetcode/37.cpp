5c20d3f69e65fb08688de47e2cfd543cea3942b2
zintrulcre <zintrulcre@gmail.com>
2020-09-15 23:57:33 +0800

    oj: lc 37

--- online-judge/leetcode/37.cpp
+++ online-judge/leetcode/37.cpp
@@ -1,88 +1,46 @@
-#include "base/pch.h"
+// #include "base/pch.h"
 
-class Solution {
-public:
-    void solveSudoku(vector<vector<char>>& board) {
-        function<bool(int, int)> CheckRow = [&](int x, int y)
-        {
-            for (int col = 0; col < 9; ++col)
-            {
-                if (col == y)
-                    continue;
-                if (board[x][col] == board[x][y])
-                    return false;
-            }
-            return true;
-        };
-
-        function<bool(int, int)> CheckCol = [&](int x, int y)
-        {
-            for (int row = 0; row < 9; ++row)
-            {
-                if (row == x)
-                    continue;
-                if (board[row][y] == board[x][y])
-                    return false;
-            }
-            return true;
-        };
-
-        function<bool(int, int)> CheckDiagnoal = [&](int x, int y)
-        {
-            for (int i = 0; i < 8; ++i)
-            {
-                int pos_x = (x + i) % 9, pos_y = (y + i) % 9;
-                if (pos_x == x && pos_y == y)
-                    continue;
-                if (board[pos_x][pos_y] == '.')
-                    continue;
-                if (board[pos_x][pos_y] == board[x][y])
-                    return false;
-            }
-            return true;
-        };
-
-        function<bool(int, int)> CheckGrid = [&](int x, int y)
-        {
-            for (int i = 0; i < 3; ++i)
-            {
-                for (int j = 0; j < 3; ++j)
-                {
-                    int pos_x = i + x / 3 * 3, pos_y = j + y / 3 * 3;
-                    if (pos_x == x && pos_y == y)
-                        continue;
-                    if (board[pos_x][pos_y] == board[x][y])
-                        return false;
-                }
-            }
-            return true;
-        };
-
-        function<bool(int, int)> Backtracking = [&](int x, int y)
-        {
-            if (x == 9)
-                return true;
+// class Solution {
+// public:
+//     void solveSudoku(vector<vector<char>>& board) {
+//         function<bool(int, int, char)> Check = [&](int x, int y, char c)
+//         {
+//             for (int i = 0; i < 9; ++i)
+//                 if (board[x][i] == c || board[i][y] == c)
+//                     return false;
             
-            auto NextIter = bind(Backtracking, x + (y + 1) / 9, (y + 1) % 9);
+//             for (int i = 0; i < 3; ++i)
+//                 for (int j = 0; j < 3; ++j)
+//                     if (board[i + x / 3 * 3][j + y / 3 * 3] == c)
+//                         return false;
 
-            cout << x << ' ' << y << endl;
-            if (board[x][y] != '.')
-                return NextIter();
+//             return true;
+//         };
+
+//         function<bool(int, int)> Backtracking = [&](int x, int y)
+//         {
+//             if (x == 9)
+//                 return true;
             
-            for (char c = '0'; c < '9'; ++c)
-            {
-                board[x][y] = c;
-                if (CheckRow(x, y) && CheckCol(x, y) && CheckDiagnoal(x, y) && CheckGrid(x, y))
-                {
-                    leadgrey::Print(board);
-                    if (NextIter())
-                        return true;
-                }
-            }
+//             auto NextIter = bind(Backtracking, x + (y + 1) / 9, (y + 1) % 9);
 
-            return false;
-        };
+//             if (board[x][y] != '.')
+//                 return NextIter();
+            
+//             for (char c = '1'; c <= '9'; ++c)
+//             {
+//                 if (Check(x, y, c))
+//                 {
+//                     board[x][y] = c;
+//                     if (NextIter())
+//                         return true;
+//                     board[x][y] = '.';
+//                 }
+//             }
 
-        Backtracking(0, 0);
-    }
-};
+//             return false;
+//         };
+
+//         Backtracking(0, 0);
+//     }
+// };
