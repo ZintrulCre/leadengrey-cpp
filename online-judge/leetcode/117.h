c337d44b086023b59824634da6bdf2e42b60aa4f
zintrulcre <zintrulcre@gmail.com>
2020-09-28 12:09:09 +0800

    oj: lc 117

---
+++ online-judge/leetcode/117.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "universal/std-pch.h"
+#include "online-judge/pch.h"
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node*> que;
+        if (root)
+            que.push(root);
+        while ((int m = que.size()) != 0)
+        {
+            for (int i = m - 1; i >= 0; --i)
+            {
+                Node* node = que.front();
+                que.pop();
+                if (!que.empty() && i != 0)
+                    node->next = que.front();
+                if (node->left)
+                    que.push(node->left);
+                if (node->right)
+                    que.push(node->right);
+            }
+        }
+        return root;
+    }
+};
