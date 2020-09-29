7a4d8f86eead4c711691ac47b92ff5b94f463c93
zintrulcre <zintrulcre@gmail.com>
2020-09-29 11:15:01 +0800

    lc: oj 145

---
+++ online-judge/leetcode/145.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "universal/std-pch.h"
+#include "online-judge/pch.h"
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        function<void(TreeNode*)> PostorderTraverse = [&](TreeNode* node) -> void
+        {
+            if (!node)
+                return;
+            PostorderTraverse(node->left);
+            PostorderTraverse(node->right);
+            res.emplace_back(node->val);
+        };
+        PostorderTraverse(root);
+        return res;
+    }
+};
