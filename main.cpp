a908dcb2832f1a298dd556b620979ab1ebe192ca
zintrulcre <zintrulcre@gmail.com>
2021-04-16 21:09:40 +0800

    chore: compile option diagnostics-color

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "universal/std-pch.h"
-// #include "online-judge/pch.h"
+#include "online-judge/pch.h"
 #include "test-case/pch.h"
 
 int main()
 {
-    // OjTest();
+    OjTest();
     // AddressSanitizerTest();
-    AutoPointerFoo();
+    // AutoPointerFoo();
     // UniquePointerFoo();
     // MemFnTest();
 }
--- online-judge/leetcode/test.h
+++ online-judge/leetcode/test.h
@@ -32,13 +32,13 @@ void TreeNodeTest()
 
 }
 
-void ConcurrencyTest()
-{
-    Foo foo;
-    foo.first();
-    foo.second();
-    foo.third();
-}
+// void ConcurrencyTest()
+// {
+//     Foo foo;
+//     foo.first();
+//     foo.second();
+//     foo.third();
+// }
 
 void OjTest()
 {
@@ -52,6 +52,28 @@ void OjTest()
     // vector<vector<int>> res = solution.subsets(test);
     // leadgrey::Print(res);
 
+
+    int N;
+    cin >> N;
+    for (int n = 0; n < N; ++n)
+    {
+        int m;
+        cin >> m;
+        vector<int> v(m);
+        int res = 0;
+        for (int i = 0; i < m; ++i)
+        {
+            cin >> v[i];
+            if (v[i] > 0)
+                ++res;
+            else if (v[i] < 0)
+                --res;
+            else
+                res += (res > 0 ? -1 : 1);
+            
+        }
+        cout << (abs(res) <= 1 ? "Yes" : "No") << endl;
+    }
 }
 
 #endif
