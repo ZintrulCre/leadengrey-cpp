511298dc8e452a7388e80a48c2aeb8e3d825084a
zintrulcre <zintrulcre@gmail.com>
2021-04-19 20:12:58 +0800

    structure: split smart-pointer

--- test-case/vla.h
+++ test-case/vla.h
@@ -3,6 +3,9 @@
 #include "universal/std-pch.h"
 #include "universal/boost-pch.h"
 
+// https://en.cppreference.com/w/c/language/array
+// variable-length arrays
+
 void VlaTest(int n)
 {
     int a[n];
