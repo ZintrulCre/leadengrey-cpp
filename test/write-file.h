7a5af64f0bcb5a5cab20cdc4ecf60a1e07755498
zintrulcre <zintrulcre@gmail.com>
2021-06-09 10:06:47 +0800

    update: write file

--- test/write-file.h
+++ test/write-file.h
@@ -11,12 +11,14 @@ void StdFstream()
 
 void LinuxSystemCall()
 {
+	pid_t pid = getpid();
+
 	char buf[20];
-	strcpy(buf, "success");
+	sprintf(buf, "%d", pid);
 	size_t nbytes = strlen(buf);
 
 	int fd;
-	fd = open("initialization.status", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	fd = open("pid", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	write(fd, buf, nbytes);
 	close(fd);
 }
