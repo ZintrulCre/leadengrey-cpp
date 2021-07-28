447712fe114b1fb64ffc6af2440190b82a4f8c72
zintrulcre <zintrulcre@gmail.com>
2021-07-28 23:20:22 +0800

    feat: leetcode 124

--- online-judge/leetcode/124.h
+++ online-judge/leetcode/124.h
@@ -9,7 +9,6 @@ private:
 public:
     int maxPathSum(TreeNode* root) {
         maxPathSumImpl(root);
-        cout << res << endl;
         return res;
     }
     
@@ -18,9 +17,7 @@ public:
             return 0;
         int left_val = max(0, maxPathSumImpl(node->left));
         int right_val = max(0, maxPathSumImpl(node->right));
-        cout << left_val << ' ' << right_val << endl;
         res = max(res, left_val + node->val + right_val);
-        cout << res << endl;
         return max(left_val, right_val) + node->val;
     }
 };
