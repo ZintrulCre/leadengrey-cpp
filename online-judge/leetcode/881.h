e42d4c2896322c792468e5fa420a72a10c06cf39
zintrulcre <zintrulcre@gmail.com>
2021-08-26 20:19:51 +0800

    feat: leetcode 881

---
+++ online-judge/leetcode/881.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "online-judge/pch.h"
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& p, int l) {
+        std::sort(p.begin(), p.end());
+        int n = p.size(), i = 0, j = n - 1, res = 0;
+        while (i < j) {
+            if (p[i] + p[j] <= l)
+                ++i;
+            --j;
+            ++res;
+        }
+        if (i == j)
+            ++res;
+        return res;
+    }
+};
