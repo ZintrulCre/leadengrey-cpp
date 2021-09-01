3b18a67b0fc7f8627435db3fd31968b00197693a
zintrulcre <zintrulcre@gmail.com>
2021-09-01 21:36:21 +0800

    feat: leetcode 165

--- online-judge/leetcode/165.h
+++ online-judge/leetcode/165.h
@@ -12,24 +12,23 @@
 class Solution {
 public:
     int compareVersion(string s1, string s2) {
-        int x1, x2;
-        s1+='.', s2+='.';
-        while (!s1.empty() || !s2.empty()) {
-            GetSub(s1, x1);
-            GetSub(s2, x2);
-            if (x1 > x2)    return 1;
-            if (x1 < x2)    return -1;
+        int i1 = 0, i2 = 0;
+        while (i1 < s1.size() || i2 < s2.size()) {
+            int v1 = GetSub(s1, i1), v2 = GetSub(s2, i2);
+            if (v1 > v2)    return 1;
+            if (v1 < v2)    return -1;
         }
         return 0;
     }
 
-    void GetSub(string& s, int& x) {
-        int p = s.find(".");
-        string temp = p == -1 ? "0" : s.substr(0, p);
-        s = s.substr(p+1);
-        while (temp.size() > 1 && temp[0] == '0')
-            temp = temp.substr(1);
-        x = std::stoi(temp);
+    int GetSub(string& s, int& i) {
+        int n = s.size(), v = 0;
+        while (i < n && s[i] != '.') {
+            v = v*10+s[i]-'0';
+            ++i;
+        }
+        ++i;
+        return v;
     }
 };
 // @lc code=end
