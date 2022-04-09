4e0ba82e0890eba0363141793714a90afb5435f9
zintrulcre <zintrulcre@gmail.com>
2022-04-09 12:31:48 +0800

    feat: rename java to maven

--- java/README.md
+++
@@ -1,3 +0,0 @@
-mvn compile
-
-mvn exec:java -Dexec.mainClass="com.Providence.app.App"
--- java/src/main/java/com/Providence/app/designpattern/builder/Builder.java
+++
@@ -1,7 +0,0 @@
-package com.Providence.app.designpattern.builder;
-
-public class Builder {
-    public Game buildWoW() {
-        return new Game("Wow", 75);
-    }
-}
